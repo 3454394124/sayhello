e3de6e246005b5e09d72203243ae5c3d9ad89093
3454394124 <3454394124@qq.com>
2019-04-16 17:36:05 +0800

    string

---
+++ hellopy/stringpass.py
@@ -0,0 +1,9 @@
+str = input('请输入：')
+
+print(str.isalnum()) # 判断所有字符都是数字或者字母
+print(str.isalpha())# 判断所有字符都是字母
+print(str.isdigit())
+print(str.islower())
+print(str.isupper())
+print(str.istitle()) # 判断所有单词都是首字母大写，像标题
+print(str.isspace())# 判断所有字符都是空白字符、\t、\n、\r
